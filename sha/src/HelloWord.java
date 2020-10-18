9f0d5940881d42694529aac585be60e10aa192d2
tanzhenxing <1296780581@qq.com>
2020-10-18 21:51:01 +0800

    添加sha这个Java项目

---
+++ sha/src/HelloWord.java
@@ -0,0 +1,11 @@
+public class HelloWord {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("helloworld");
+        Demo1 demo1=new Demo1();
+        demo1.sleep(2);
+        System.out.println("hahhaha");
+
+    }
+
+
+}
